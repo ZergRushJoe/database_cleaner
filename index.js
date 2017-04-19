4ab0c619cb33272bafed8b8f2911155b8d1ae87c
Joseph Karp <joe12345hd@yahoo.com>
2017-04-19 14:18:11 -0400

    add package.json
    made the version one of the code

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 /**
  * Created by joe12 on 4/19/2017.
  */
+class cleaner
+{
+    constructor()
+    {
+
+    }
+    mongoClean(string)
+    {
+        return string.replace(/[{}"'/]/g)
+    }
+    sqlClean(string)
+    {
+        return string.replace(/[;']/g)
+    }
+    hash(string)
+    {
+        let temp;
+        for(let i =0;i<string.length;i++)
+        {
+            temp += (temp.charCodeAt(i)*i%89) + ""
+        }
+    }
+}
